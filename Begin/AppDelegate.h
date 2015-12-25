940a7a09f48484fb9c3bf4380291c3ee9e45de60
WangHeYi <15345517626@163.com>
2015-12-25 08:56:05 +0800

    Initial Commit

---
+++ Begin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Begin
+//
+//  Created by apple on 15/12/25.
+//  Copyright © 2015年 WangHeYi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Begin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Begin
+//
+//  Created by apple on 15/12/25.
+//  Copyright © 2015年 WangHeYi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
